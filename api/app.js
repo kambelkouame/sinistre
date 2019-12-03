fe90a05f93607e0c45f09327c3de1f5383c29446
oikambelkouame@gmail.com <oikambelkouame@gmail.com>
2019-12-03 10:30:05 +0000

    version en marche

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+//Mes dependences du server
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,5 +48,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(9000, ()=>{
+	console.log("appli en marche")
+} )
 module.exports = app;
--- api/routes/process.js
+++ api/routes/process.js
@@ -1,79 +1,103 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 var router = express.Router();
-var mysql = require('mysql')
-var db= require('../database/db')
-var fetch =require('node-fetch')
-let checkstatus = require('checkstatus')
-let parseJSON = require('parse-json')
-let logError = require('log-error')
+var mysql = require('mysql');
+var db= require('../database/db');
+var fetch =require('node-fetch');
+let checkstatus = require('checkstatus');
+let parseJSON = require('parse-json');
+let logError = require('log-error');
+var cors = require("cors");
 
 
+//insertion d'une declaration de sinistre
 
-router.post("/", (req, res) => {
-	let sql ="INSERT INTO reclamation  SET ? "
+router.post("/", function(req, res){
+
+	let sql ="INSERT INTO reclamation  SET ? ";
     const type_sinistre = req.body.type_sinistre;
     const ville = req.body.ville;
     const assurance = req.body.assurance;
     const commune = req.body.commune;
     const immatriculation = req.body.immatriculation;
 
-let sinistre = {
-	type_sinistre:type_sinistre,
-	ville:ville,
-	assurance:assurance,
-	commune:commune,
-	immatriculation:immatriculation,
-   }
+     
 
-   //connexion a la base de Donnée
+        //variable d'insertion des sinistres
+		
+		 
+		let sinistre = {
+			type_sinistre:type_sinistre,
+			ville:ville,
+			assurance:assurance,
+			commune:commune,
+			immatriculation:immatriculation,
+		   }
 
-let connexion = mysql.createConnection({
-  host : db.hostname,
-  user : db.username,
-  password : db.password,
-  database : db.dbname,
-  port : db.port,
-})
+	console.log(sinistre);
+       
+         //connexion a la base de Donnée
 
-console.log(sinistre);
+		let connexion = mysql.createConnection({
+		  host : db.hostname,
+		  user : db.username,
+		  password : db.password,
+		  database : db.dbname,
+		  port : db.port,
+		})
 
-connexion.query(sql,sinistre) ,( err,result)=>{
-	if(err){
-		console.log(err.message)
-	}
-}
+ 		
+
+		//action d'insertion dans la base de donnée
+		
+		connexion.query(sql,sinistre) ,( err,result)=>{
+			if(err){
+				console.log(err.message)
+			}
+		}
+
+		 
+         //Variable contenant les données a inserer dans la blockchain
+
+		 let donneechain ={
+		   	"sender":type_sinistre,
+		   	"receiver":assurance,
+		   	"amount":1,
+
+		   }
+
+          //transformation de donneechain en format JSON
+
+		  donnechain=JSON.stringify(donneechain)
+		 
+           //Affichage de  donnechain dans le terminal
+
+		  console.log(donnechain) 
 
 
-/*
-  let donneechain ={
-   	"sender":type_sinistre,
-   	"receiver":assurance,
-   	"amount":2,
+		   const urls=[
+		   'http://localhost:5000/transaction',
+		   'http://localhost:5001/transaction'
+		   ];
+          let url1='http://localhost:5001/transaction';
 
-   }
+		 
+		   	fetch(url1,
+		   	  {
+		   	  method: "POST",
+		   	  headers: {
+			      'Accept': 'application/json',
+			      'Content-Type': 'application/json'
+					    },
+		      body: donnechain})
+		  // 	.then(checkstatus.checkStatus(url))
+		   	//.then(donnee => JSON.stringify(donnee))
+		  // 	.catch(err => console.log(err.message))
+		   .then(data => {
+		   	console.log("retour blockchain", data)
+		   })
 
-  donnechain=json.stringify(donneechain)
-  console.log(donnechain) 
-   const urls=[
-   'http://localhost:5000/transaction',
-   'http://localhost:5001/transaction'
-   ];
 
-   Promise.all(urls.map(url =>
-   	fetch(url,
-   	  {
-   	  method: "post",
-      body: donnechain})
-   	.then(checkstatus.checkStatus(url))
-   	//.then(donnee => JSON.stringify(donnee))
-  // 	.catch(err => console.log(err.message))
-   	))
-   .then(data => {
-   	console.log("retour blockchain", data)
-   })
-
-*/
 
 })
 
